f1d03227272c5f335c5335a90cc59c9d537fc68f
HashEngineering <hashengineeringsolutions@gmail.com>
2023-07-24 08:12:09 -0700

    more for uniffi

--- uniffi/src/main/rust/src/identity/identity.rs
+++ uniffi/src/main/rust/src/identity/identity.rs
@@ -1,9 +1,14 @@
-use std::sync::Arc;
-use dpp::identity::{Identity, KeyType, Purpose, SecurityLevel};
+use std::cell::RefCell;
+use std::sync::{Arc, atomic, RwLock};
+use std::borrow::Borrow;
+use dpp::identity::{Identity, IdentityPublicKey, KeyType, Purpose, SecurityLevel};
 use dpp::prelude::Revision;
+use dpp::ProtocolError;
 use dpp::serialization_traits::PlatformDeserializable;
 use crate::identifier::identifier::IdentifierWrapper;
-use crate::errors::protocol_error::ProtocolErrorWrapper;
+use crate::errors::protocol_error::ProtocolErrorType;
+use core::sync::atomic::AtomicPtr;
+use crate::IdentityPublicKeyWrapper;
 
 pub struct IdentityWrapper {
     pub identity: Identity,
@@ -27,29 +32,40 @@ impl IdentityWrapper {
 
     /// Set Identity balance
     pub fn set_balance(&mut self, balance: u64) {
-        self.set_balance(balance);
+        self.identity.set_balance(balance);
     }
 
     /// Increase Identity balance
     pub fn increase_balance(&mut self, amount: u64) -> u64 {
-        self.increase_balance(amount)
+        self.identity.increase_balance(amount)
     }
 
     /// Reduce the Identity balance
     pub fn reduce_balance(&mut self, amount: u64) -> u64 {
-        self.reduce_balance(amount)
+        self.identity.reduce_balance(amount)
     }
 
     /// Get Identity revision
     pub fn get_revision(&self) -> Revision {
-        self.get_revision()
+        self.identity.get_revision()
+    }
+    pub fn get_public_key_max_id(&self) -> u32 {
+        self.identity.get_public_key_max_id() as u32
+    }
+
+    /// Returns a public key for a given id
+    pub fn get_public_key_by_id(&self, key_id: u32) -> Option<Arc<IdentityPublicKeyWrapper>> {
+        match self.identity.get_public_key_by_id(key_id) {
+            Some(identity_public_key) => Some(Arc::new(identity_public_key.clone().into())),
+            None => None
+        }
     }
 
     /// Increment revision
-    pub fn increment_revision(&mut self) -> Result<(), ProtocolErrorWrapper> {
-        match self.increment_revision() {
+    pub fn increment_revision(&mut self) -> Result<(), ProtocolErrorType> {
+        match self.identity.increment_revision() {
             Ok(..) => Ok(()),
-            Err(error) => Err(error.into())
+            Err(error) => Err(ProtocolErrorType::ConsensusError)
         }
     }
 }
--- uniffi/src/test/kotlin/uniffi/kotlin_dpp/test.kt
+++ uniffi/src/test/kotlin/uniffi/kotlin_dpp/test.kt
@@ -1,8 +1,8 @@
 package uniffi.kotlin_dpp
 
 import org.dashj.platform.dpp.util.Converters
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-
 
 fun ByteArray.toUByteList(): List<UByte> {
     return toList().map { it.toUByte() }
@@ -13,12 +13,15 @@ class Test {
         println("my test")
         val me = IdentifierWrapper(ByteArray(32).toUByteList())
         me.destroy()
-        println(me.toBase58())
+        // println(me.toBase58())
 
-        val bytes = Converters.fromHex("0120413f39f3cc096a47bb025eddbe7f8d7289d1f3323cd75a65e50f73052c75b6d9020000000000002103292da1684067c3b78ecef3d3e2c132434e7aa9930c0a520bfc4e2ad73a3edd6b0001010002000021020a066d33abcd94c00f01fc84ae3376f7103c79d8dbacba2b4b20e0eb733ec0150000000000");
+        val bytes = Converters.fromHex("0120413f39f3cc096a47bb025eddbe7f8d7289d1f3323cd75a65e50f73052c75b6d9020000000000002103292da1684067c3b78ecef3d3e2c132434e7aa9930c0a520bfc4e2ad73a3edd6b0001010002000021020a066d33abcd94c00f01fc84ae3376f7103c79d8dbacba2b4b20e0eb733ec0150000000000")
         val you = IdentityWrapper.deserialize(bytes.toUByteList())
-        val you2 = IdentityWrapper(bytes.toUByteList())
-        //println(you.getId().toString())
+        assertEquals(1u, you.getProtocolVersion())
+        val ipk = you.getPublicKeyById(0u)
+        // val ipk;
+        // val you2 = IdentityWrapper(bytes.toUByteList())
+        // println(you.getId().toString())
         println(you.getId().toBase58())
         you.destroy()
     }
